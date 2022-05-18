a659b0baff076a1a81cfe0182d1ad8e642e4c097
musaberkay <79651197+musaberkay@users.noreply.github.com>
2022-05-19 02:04:37 +0300

    Scapy sniffing function added.

--- scapy_sniffer.py
+++ scapy_sniffer.py
@@ -1,11 +1,28 @@
 from scapy.all import *
 
-def custom_action(packet):
-    print("asd")
-    print(packet)
 
-def start_sniff(count_, interface):
-    return sniff(count=count_, iface=interface, prn=custom_action)
+def start_sniff(count_, save_pcap, interface, result_, index_):
 
-a = start_sniff(5, "enp0s3")
-print(a)
+    processed_data = []
+    start_time = time.time()
+    packets = sniff(iface=interface, count=count_)
+    count=0
+
+    for packet in packets:
+        if count==0:
+            data_info = {"Time(ms)": str(round((packet.time-start_time)*1000, 2))}
+        else:
+            data_info = {"Time(ms)": str(round((packets[count].time-packets[count-1].time)*1000, 2))}
+        data_info["Source IP"] = packet[IP].src
+        data_info["Destination IP"] = packet[IP].dst
+        data_info["Packet Length"] = str(len(packet))
+        data_info["Protocol"] = packet.getlayer(2).name
+        data_info["Details"] = packet.show(dump=True)
+        processed_data.append(data_info)
+        count += 1
+
+    if save_pcap:
+        wrpcap("scapy_capture.pcap", packets)
+    
+    result_[index_] = processed_data
+    return processed_data
